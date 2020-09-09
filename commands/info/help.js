e32fb364dae514345311c6b6f08fd55ec23c9608
SlacicMan <sm8079@tmc.catholic.edu.au>
2020-09-09 21:30:15 +0930

    Making Changes

--- commands/info/help.js
+++ commands/info/help.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
     if(command[1] == undefined){
         const embed = new Discord.MessageEmbed()
             .setColor("#FF4500")
-            .addField("Commands",`You can view commands at [AtomicBots](https://http://atomicbots.glitch.me/commands.html)`);
+            .addField("Commands",`You can view commands at [AtomicBots](http://atomicbots.glitch.me/commands.html)`);
         message.channel.send(embed);    
     }
 
